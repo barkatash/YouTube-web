fdb0b1e46c50ea2efe83b2a1321ef83258e5527b
Your Name <you@example.com>
2024-05-29 17:07:12 +0300

    homepage changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
 import './navbar/Navbar.js';
 import Navbar from './navbar/Navbar.js';
+import Sidebar from './sidebar/Sidebar.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container-fluid text-center">
+        <div className="row">
+          <div className="col-2">
+            <Sidebar />
+          </div>
+          <div className="col-10"></div>
+        </div>
+      </div>
     </div>
   );
 }
