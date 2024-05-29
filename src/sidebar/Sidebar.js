88be5d289ee64316b4c419cf16c95baaa71cda01
Your Name <you@example.com>
2024-05-29 17:13:27 +0300

    homepage

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -3,6 +3,12 @@ import home from '../images/1946488.png'
 import explore from '../images/magnifying-glass.svg';
 import tranding from '../images/hot_fire_flame_trend_creative_trending_icon_212616.png'
 import music from '../images/music.png'
+import sports from '../images/sport.png'
+import news from '../images/news.png'
+import like from '../images/like.png'
+import history from '../images/history.png'
+import channel from '../images/channel.png'
+import videos from '../images/video.png'
 
 function Sidebar() {
   return (
@@ -24,11 +30,11 @@ function Sidebar() {
           <span className="badge text-bg-primary rounded-pill">1</span>
         </li>
         <li className="list-group-item d-flex justify-content-between align-items-center">
-        <img className="small-logo" src={explore}></img>News
+        <img className="small-logo" src={news}></img>News
           <span className="badge text-bg-primary rounded-pill">1</span>
         </li>
         <li className="list-group-item d-flex justify-content-between align-items-center">
-        <img className="small-logo" src={explore}></img>Sports
+        <img className="small-logo" src={sports}></img>Sports
           <span className="badge text-bg-primary rounded-pill">1</span>
         </li>
         <hr></hr>
@@ -37,19 +43,19 @@ function Sidebar() {
           <span className="badge text-bg-primary rounded-pill">14</span>
         </li>
         <li className="list-group-item d-flex justify-content-between align-items-center">
-        <img className="small-logo" src={explore}></img>Your channel
+        <img className="small-logo" src={channel}></img>Your channel
           <span className="badge text-bg-primary rounded-pill">2</span>
         </li>
         <li className="list-group-item d-flex justify-content-between align-items-center">
-        <img className="small-logo" src={explore}></img>History
+        <img className="small-logo" src={history}></img>History
           <span className="badge text-bg-primary rounded-pill">1</span>
         </li>
         <li className="list-group-item d-flex justify-content-between align-items-center">
-        <img className="small-logo" src={explore}></img>Your videos
+        <img className="small-logo" src={videos}></img>Your videos
           <span className="badge text-bg-primary rounded-pill">14</span>
         </li>
         <li className="list-group-item d-flex justify-content-between align-items-center">
-        <img className="small-logo" src={explore}></img>Liked videos
+        <img className="small-logo" src={like}></img>Liked videos
           <span className="badge text-bg-primary rounded-pill">2</span>
         </li>
         <hr></hr>
