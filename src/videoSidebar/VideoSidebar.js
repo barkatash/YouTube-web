e4bb711a1089a9a14dd05068044db18aef3e7796
Your Name <you@example.com>
2024-06-02 20:33:57 +0300

    video side bar

--- src/videoSidebar/VideoSidebar.js
+++ src/videoSidebar/VideoSidebar.js
@@ -1,9 +1,20 @@
 import './VideoSidebar.css'
-function VideoSidebar() {
+function VideoSidebar( { image, title, uploader, visits, duration, uploadDate } ) {
     return (
-        <div>
-
-        </div>
-    )
+        <div className="container mt-3">
+            <div className="d-flex align-items-center">
+                <img src={image} className="img-fluid" alt="Image Description" style={{ maxWidth: '30%' }} />
+                <div className="ml-3">
+                <p className="card-title mb-0">{title}</p>
+                <p className="card-text mb-0">{uploader}</p>
+                <p className="card-text">
+                    <small className="text-muted">
+                    {visits} • {uploadDate}
+                    </small>
+                </p>
+                </div>
+            </div>
+            </div>
+    );
 }
 export default VideoSidebar;
