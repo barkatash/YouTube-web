f7bfa8d5efceb0fb53b85c96d50f9b601f0fd397
Your Name <you@example.com>
2024-06-03 10:49:43 +0300

    bar

--- src/Homepage.js
+++ src/Homepage.js
@@ -6,25 +6,19 @@ import './Homepage.css'
 
 function Homepage({matchedVideos}) {
     return (
-    //     <div>
-    //     <div className="row">
-    //     <div className="col-md-2">
-    //     <Sidebar />
-    //     </div>
-    //     <div className="col-md-10">
-    //     <div className="row">
-    //     <CategoryNavbar categories={categories}/>
-    //     </div>
-    //         <VideoList matchedVideos={matchedVideos}/></div>
-    //         </div>
-    //   </div>
-    <div class="row">
-    <div class="col-2"><Sidebar /></div>
-    <div class="col-10"><CategoryNavbar categories={categories}/>
-    <VideoList matchedVideos={matchedVideos}/>
-    </div>
-    <div class="w-100"></div>
-    </div>
-    )
+      <div>
+        <div className="row">
+          <div className="col-md-2">
+            <Sidebar />
+          </div>
+          <div className="col-md-10">
+            <div className="row">
+              <CategoryNavbar categories={categories} />
+            </div>
+            <VideoList matchedVideos={matchedVideos} />
+          </div>
+        </div>
+      </div>
+    );
 }
 export default Homepage;
