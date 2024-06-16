9de4f4f1dc8e1a5975069d6b3f31a939d1294ead
Your Name <you@example.com>
2024-06-16 18:10:48 +0300

    video design

--- src/videoSidebar/VideoSidebar.js
+++ src/videoSidebar/VideoSidebar.js
@@ -16,11 +16,11 @@ function VideoSidebar( { id, image, title, uploader, visits, duration, uploadDat
         <div className="d-flex align-items-start position-relative">
           <img
             src={isFromDb(id) ? `${process.env.PUBLIC_URL}/${image}` : image}
-            className="img-fluid"
+            className="img-fluid video-img"
             alt="Description"
             onClick={changeWatchedVideo}
           />
-          <span className="duration">{duration}</span>
+          <span className="duration-side-video">{duration}</span>
           <div className="ml-3 ml-3-video-side-bar">
             <p className="card-title-sidebar mb-0">{title}</p>
             <p className="card-text-sidebar text-muted">{uploader}</p>
