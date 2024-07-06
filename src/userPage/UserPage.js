cde794fc22092646d99d547585a98cf4f5a3ae3c
Your Name <you@example.com>
2024-07-06 23:22:03 +0300

    userPage

--- src/userPage/UserPage.js
+++ src/userPage/UserPage.js
@@ -1,43 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import "./UserPage.css"
-import { useNavigate } from "react-router-dom";
-import { daysAgo } from "../video/utils";
+import { useNavigate } from 'react-router-dom';
+import './UserPage.css';
+import { daysAgo } from '../video/utils';
 
 function UserPage() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [userVideos, setUserVideos] = useState([]);
 
-  const fetchUserVideos = async () => {
+  const fetchUserVideos = useCallback(async () => {
     try {
       const response = await fetch(`http://localhost:8080/api/users/${id}/videos`);
       const data = await response.json();
       setUserVideos(data);
     } catch (error) {
-      console.error("Error fetching user videos:", error);
+      console.error('Error fetching user videos:', error);
     }
-  };
-  const onMoveToVideo = (id) => {
-    navigate(`/watch/${id}`);
+  }, [id]);
+
+  const onMoveToVideo = (videoId) => {
+    navigate(`/watch/${videoId}`);
   };
 
   useEffect(() => {
     fetchUserVideos();
-  }, [id]);
+  }, [fetchUserVideos]);
+
   return (
     <div className="user-page">
       <header className="user-header">
         <h1>{id}</h1>
       </header>
-      <hr></hr>
-      <h5 className='for-you-videos'>For You</h5>
-      <br></br>
+      <hr />
+      <h5 className="for-you-videos">For You</h5>
+      <br />
       <div className="video-grid">
         {userVideos && userVideos.map((video) => (
           <div key={video._id} className="video-card">
             <div className="position-relative">
-              <img src={`http://localhost:8080/${video.image}`} className="video-user-image" alt={video.title} onClick={() => onMoveToVideo(video._id)}/>
+              <img
+                src={`http://localhost:8080/${video.image}`}
+                className="video-user-image"
+                alt={video.title}
+                onClick={() => onMoveToVideo(video._id)}
+              />
               <span className="duration-user-video">{video.duration}</span>
             </div>
             <div className="video-info">
@@ -47,7 +54,7 @@ function UserPage() {
             </div>
           </div>
         ))}
-        {!userVideos && (<p>No videos found</p>)}
+        {!userVideos.length && <p>No videos found</p>}
       </div>
     </div>
   );
